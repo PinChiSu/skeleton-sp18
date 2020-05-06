f6ead1d86ea5bdb06cf1327fbad0656ac14853ff
PinChi <sueric68@gmail.com>
2020-05-07 01:53:00 +0800

    proj1b finished

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -4,19 +4,18 @@ import static org.junit.Assert.*;
 public class TestOffByOne {
     /*
     // You must use this CharacterComparator and not instantiate
-    // new ones, or the autograder might be upset.
+    // new ones, or the autograder might be upset.*/
     static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
-     **/
+    
     @Test
     public void testEqualChars() {
-        OffByOne offbyone = new OffByOne();
-        assertFalse(offbyone.equalChars('a', 'a'));
-        assertFalse(offbyone.equalChars('a', 'A'));
-        assertFalse(offbyone.equalChars('a', 'z'));
-        assertTrue(offbyone.equalChars('a', 'b'));
-        assertTrue(offbyone.equalChars('r', 'q'));
-        assertFalse(offbyone.equalChars('a', 'B'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'A'));
+        assertFalse(offByOne.equalChars('a', 'z'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertFalse(offByOne.equalChars('a', 'B'));
     }
 }
