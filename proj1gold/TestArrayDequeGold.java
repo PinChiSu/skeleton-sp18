1802ed90695d076e025078470a3ba778d537097b
PinChi <sueric68@gmail.com>
2020-05-08 23:40:49 +0800

    proj1 gold modified.

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -15,26 +15,22 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sol1 = new ArrayDequeSolution<>();
         StringBuilder steps = new StringBuilder();
-        for (int i = 0; i < 10; i += 1) {
+        for (int i = 0; i < 100; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.25) {
                 sad1.addLast(i);
                 sol1.addLast(i);
                 steps.append("addLast(").append(i).append(")\n");
-                int actual = sad1.removeLast();
-                int expected = sol1.removeLast();
+                int actual = sad1.get(sad1.size() - 1);
+                int expected = sol1.get(sad1.size() - 1);
                 assertEquals(steps.toString(), expected, actual);
-                sad1.addLast(i);
-                sol1.addLast(i);
             } else if (numberBetweenZeroAndOne < 0.5 && numberBetweenZeroAndOne > 0.25) {
                 sad1.addFirst(i);
                 sol1.addFirst(i);
                 steps.append("addFirst(").append(i).append(")\n");
-                int actual = sad1.removeFirst();
-                int expected = sol1.removeFirst();
+                int actual = sad1.get(0);
+                int expected = sol1.get(0);
                 assertEquals(steps.toString(), expected, actual);
-                sad1.addFirst(i);
-                sol1.addFirst(i);
             } else if (numberBetweenZeroAndOne < 0.75 && numberBetweenZeroAndOne > 0.5) {
                 if (sol1.size() != 0) {
                     //sad1.removeFirst();
@@ -42,16 +38,14 @@ public class TestArrayDequeGold {
                     int actual = sad1.removeFirst();
                     int expected = sol1.removeFirst();
                     steps.append("removeFirst()\n");
-                    assertEquals(steps.toString(), expected, 25);
+                    assertEquals(steps.toString(), expected, actual);
                 } else {
                     sad1.addFirst(i);
                     sol1.addFirst(i);
                     steps.append("addFirst(").append(i).append(")\n");
-                    int actual = sad1.removeFirst();
-                    int expected = sol1.removeFirst();
+                    int actual = sad1.get(0);
+                    int expected = sol1.get(0);
                     assertEquals(steps.toString(), expected, actual);
-                    sad1.addFirst(i);
-                    sol1.addFirst(i);
                 }
             } else {
                 if (sol1.size() != 0) {
@@ -63,14 +57,11 @@ public class TestArrayDequeGold {
                     sad1.addLast(i);
                     sol1.addLast(i);
                     steps.append("addLast(").append(i).append(")\n");
-                    int actual = sad1.removeLast();
-                    int expected = sol1.removeLast();
+                    int actual = sad1.get(sad1.size() - 1);
+                    int expected = sol1.get(sol1.size() - 1);
                     assertEquals(steps.toString(), expected, actual);
-                    sad1.addLast(i);
-                    sol1.addLast(i);
                 }
             }
         }
     }
-
 }
