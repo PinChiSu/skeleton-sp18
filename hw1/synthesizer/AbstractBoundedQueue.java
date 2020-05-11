f6de74ba5509a3e5c75a0e0f8f4392d6cb392c3f
PinChi <sueric68@gmail.com>
2020-05-11 14:12:23 +0800

    HW1 finished

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,6 +1,10 @@
 package synthesizer;
 
 public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    /**這次HW中abstract class的目的是把interface不能裝的變數放進來，再讓ArrayRB去extend，
+     * 而autoGrader不希望我們用abstract method(abstract method的東西一定要被Override)去複寫enqueue等API
+     * ，其實還是可以這麼做，但是有點多此一舉。
+     */
     protected int fillCount;
     protected int capacity;
     public int capacity() {
